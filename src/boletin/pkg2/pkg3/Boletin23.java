b9287be5cee3828653d5c726904454c4a4f02b9a
dcuerdogarcia <dcuerdogarcia@eniac10.danielcastelao.org>
2014-10-07 09:27:22 +0200

    Commit inicial, usando Git

---
+++ src/boletin/pkg2/pkg3/Boletin23.java
@@ -0,0 +1,22 @@
+
+package boletin.pkg2.pkg3;
+
+import java.util.Scanner;
+
+
+public class Boletin23 {
+
+   
+    public static void main(String[] args) {
+      
+      float euros ;
+      System.out.println("euros= ");
+      Scanner dato=new Scanner(System.in);
+      euros=dato.nextFloat();
+      float dolares ;
+      System.out.println("dolares= "+euros*1.25);
+      
+    }
+}
+        
+    
